cee041b747a25db48e3cc5c0374b735fef0abffe
Volker Dörr <68968598+volkerdoerr@users.noreply.github.com>
2023-01-03 23:26:41 +0100

    Update checkout-external-components.sh

--- .github/workflows/checkout-external-components.sh
+++ .github/workflows/checkout-external-components.sh
@@ -1,8 +1,15 @@
 
 echo -----------------------------------------------------
 
-git clone --branch master https://github.com/mingodad/CocoR-CPP.git
-git clone --branch master https://github.com/pybind/pybind11
+git clone --branch master --recurse-submodules https://github.com/mingodad/CocoR-CPP
+git clone --branch master --recurse-submodules https://github.com/pybind/pybind11
+git clone --branch master --recurse-submodules https://github.com/joboccara/NamedType
+git clone --branch master --recurse-submodules https://github.com/randombit/botan
+git clone --branch master --recurse-submodules https://github.com/madler/zlib
+git clone --branch master --recurse-submodules https://github.com/skypjack/entt
+git clone --branch master --recurse-submodules https://github.com/chriskohlhoff/asio
+git clone --branch master --recurse-submodules https://github.com/libsdl-org/SDL
+git clone --branch master --recurse-submodules https://github.com/opencv/opencv
 
 echo -----------------------------------------------------
 
