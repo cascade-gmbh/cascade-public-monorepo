c68dda519226e6166f4742beac11d1b03397e5fc
Volker Dörr <68968598+volkerdoerr@users.noreply.github.com>
2023-01-04 00:13:45 +0100

    Create build-all.sh

---
+++ .github/scripts/build-all.sh
@@ -0,0 +1,6 @@
+echo build Coco/R scanner/parser generator
+echo generate DSL scanner/parser sources for all *.atg
+echo build example-dsl-cli
+echo build example-gui
+echo build example-static-library
+echo build example-dynamic-library
