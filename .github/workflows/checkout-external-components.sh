c77064ec7450b5f9f998b660ab22c6563a257f1c
Volker Dörr <68968598+volkerdoerr@users.noreply.github.com>
2023-01-03 22:36:41 +0100

    Update checkout-external-components.sh

--- .github/workflows/checkout-external-components.sh
+++ .github/workflows/checkout-external-components.sh
@@ -1,1 +1,22 @@
 
+echo -----------------------------------------------------
+
+git clone --branch master https://github.com/mingodad/CocoR-CPP.git
+git clone --branch master https://github.com/pybind/pybind11
+
+echo -----------------------------------------------------
+
+pip install numpy 
+pip install pandas
+pip install matplotlib
+pip install seaborn 
+
+echo -----------------------------------------------------
+
+pip freeze
+
+echo -----------------------------------------------------
+
+ls -R
+
+echo -----------------------------------------------------
