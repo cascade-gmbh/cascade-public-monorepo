599243e27d45b7cc7c6b945e5391e9eaa14eb3e0
Volker Dörr <gitlab@cascade.de>
2023-01-04 09:54:06 +0100

    ci script

---
+++ .github/scripts/ci.sh
@@ -0,0 +1,16 @@
+
+
+
+      - run: .github/scripts/checkout-all.sh
+        shell: bash
+      - run: .github/scripts/build-all.sh
+        shell: bash
+      - run: .github/scripts/analyze-all.sh
+        shell: bash
+      - run: .github/scripts/test-all.sh
+        shell: bash
+      - run: .github/scripts/doc-all.sh
+        shell: bash
+      - run: .github/scripts/deploy-all.sh
+        shell: bash
+
