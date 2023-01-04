9f83daa133854b0d0f2897f05e55ec401b8c9f7a
Volker Dörr <68968598+volkerdoerr@users.noreply.github.com>
2023-01-04 01:10:21 +0100

    Update checkout-all.sh

--- .github/scripts/checkout-all.sh
+++ .github/scripts/checkout-all.sh
@@ -1,15 +1,16 @@
 
 echo -----------------------------------------------------
 
-git clone --branch master --recurse-submodules https://github.com/mingodad/CocoR-CPP
-git clone --branch master --recurse-submodules https://github.com/pybind/pybind11
-git clone --branch master --recurse-submodules https://github.com/joboccara/NamedType
-git clone --branch master --recurse-submodules https://github.com/randombit/botan
-git clone --branch master --recurse-submodules https://github.com/madler/zlib
-git clone --branch master --recurse-submodules https://github.com/skypjack/entt
-git clone --branch master --recurse-submodules https://github.com/chriskohlhoff/asio
-git clone --branch main   --recurse-submodules https://github.com/libsdl-org/SDL
-git clone --branch master --recurse-submodules https://github.com/opencv/opencv
+git clone -b master --recurse-submodules https://github.com/mingodad/CocoR-CPP
+git clone -b master --recurse-submodules https://github.com/pybind/pybind11
+git clone -b master --recurse-submodules https://github.com/joboccara/NamedType
+git clone -b master --recurse-submodules https://github.com/randombit/botan
+git clone -b master --recurse-submodules https://github.com/madler/zlib
+git clone -b master --recurse-submodules https://github.com/skypjack/entt
+git clone -b master --recurse-submodules https://github.com/chriskohlhoff/asio
+git clone -b main   --recurse-submodules https://github.com/libsdl-org/SDL
+git clone -b master --recurse-submodules https://github.com/opencv/opencv
+git clone -b master --recurse-submodules https://github.com/ocornut/imgui.git
 
 echo -----------------------------------------------------
 
