c24eb8aeeea6f1a9c7195b3cce59b8f10627a2fc
Volker Dörr <68968598+volkerdoerr@users.noreply.github.com>
2023-01-06 13:33:51 +0100

    Update ci.sh
    Signed-off-by: Volker Dörr <68968598+volkerdoerr@users.noreply.github.com>

--- .github/scripts/ci.sh
+++ .github/scripts/ci.sh
@@ -1,16 +1,11 @@
 
 ls
 
-      - run: .github/scripts/checkout-all.sh
-        shell: bash
-      - run: .github/scripts/build-all.sh
-        shell: bash
-      - run: .github/scripts/analyze-all.sh
-        shell: bash
-      - run: .github/scripts/test-all.sh
-        shell: bash
-      - run: .github/scripts/doc-all.sh
-        shell: bash
-      - run: .github/scripts/deploy-all.sh
-        shell: bash
+.github/scripts/checkout-all.sh
+.github/scripts/build-all.sh
+.github/scripts/analyze-all.sh
+.github/scripts/test-all.sh
+.github/scripts/doc-all.sh
+.github/scripts/deploy-all.sh
+
 
