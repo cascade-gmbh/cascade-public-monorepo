79105a392b3db1ba95d051111fdd29b67e8da542
Volker Dörr <68968598+volkerdoerr@users.noreply.github.com>
2023-01-07 15:37:28 +0100

    Update checkout-all.sh
    Signed-off-by: Volker Dörr <68968598+volkerdoerr@users.noreply.github.com>

--- .github/scripts/checkout-all.sh
+++ .github/scripts/checkout-all.sh
@@ -8,24 +8,39 @@ echo install doxygen
 
 echo -----------------------------------------------------
 
-git clone -b master --recurse-submodules https://github.com/mingodad/CocoR-CPP
-git clone -b master --recurse-submodules https://github.com/pybind/pybind11
-git clone -b master --recurse-submodules https://github.com/joboccara/NamedType
-git clone -b master --recurse-submodules https://github.com/randombit/botan
-git clone -b master --recurse-submodules https://github.com/madler/zlib
-git clone -b master --recurse-submodules https://github.com/skypjack/entt
-git clone -b master --recurse-submodules https://github.com/chriskohlhoff/asio
-git clone -b main   --recurse-submodules https://github.com/libsdl-org/SDL
-git clone -b master --recurse-submodules https://github.com/opencv/opencv
-git clone -b master --recurse-submodules https://github.com/ocornut/imgui.git
-git clone -b master --recurse-submodules https://github.com/codeplaysoftware/computecpp-sdk.git
+git clone -b master --recurse-submodules --depth 1 https://github.com/mingodad/CocoR-CPP
+git clone -b master --recurse-submodules --depth 1 https://github.com/pybind/pybind11
+git clone -b master --recurse-submodules --depth 1 https://github.com/joboccara/NamedType
+git clone -b master --recurse-submodules --depth 1 https://github.com/randombit/botan
+git clone -b master --recurse-submodules --depth 1 https://github.com/madler/zlib
+git clone -b master --recurse-submodules --depth 1 https://github.com/skypjack/entt
+git clone -b master --recurse-submodules --depth 1 https://github.com/chriskohlhoff/asio
+git clone -b main   --recurse-submodules --depth 1 https://github.com/libsdl-org/SDL
+git clone -b master --recurse-submodules --depth 1 https://github.com/opencv/opencv
+git clone -b master --recurse-submodules --depth 1 https://github.com/ocornut/imgui.git
+git clone -b master --recurse-submodules --depth 1 https://github.com/codeplaysoftware/computecpp-sdk.git
 
 echo -----------------------------------------------------
 
-pip install numpy 
-pip install pandas
-pip install matplotlib
-pip install seaborn 
+pip install --force-reinstall -v "numpy==1.24"
+pip install --force-reinstall -v "pandas==1.5"
+pip install --force-reinstall -v "matplotlib==3.6"
+pip install --force-reinstall -v "seaborn==0.12"
+
+# contourpy==1.0.6
+# cycler==0.11.0
+# fonttools==4.38.0
+# kiwisolver==1.4.4
+# matplotlib==3.6.2
+# numpy==1.24.1
+# packaging==22.0
+# pandas==1.5.2
+# Pillow==9.4.0
+# pyparsing==3.0.9
+# python-dateutil==2.8.2
+# pytz==2022.7
+# seaborn==0.12.2
+# six==1.16.0
 
 echo -----------------------------------------------------
 
