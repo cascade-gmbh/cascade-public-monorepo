23889ec0681686345d70c194766a2e6d2e3c2304
Volker Dörr <gitlab@cascade.de>
2023-01-08 23:23:17 +0100

    Update install-all.sh

--- .github/scripts/install-all.sh
+++ .github/scripts/install-all.sh
@@ -1,3 +1,27 @@
+#!/bin/bash
+# attention: script assumes to have the repo-root-folder as current directory!
+
+# external components will be either installed 
+#   - regularily for the operationg system
+#   - or cloned into ../external relative to the repository-root directory 
+
+#---------------------------------------------------------
+
+# function to checkout external repos:
+clone_into_external() {
+  # $1: branch
+  # $2: repo-path
+  mkdir -p ../external
+  cd ../external
+  git clone -b $1 --recurse-submodules --depth 1 $2 
+}
+
+#---------------------------------------------------------
+
+# temporarily go one up:
+mkdir ../external
+cd ../external
+ls
 
 echo -----------------------------------------------------
 
@@ -8,24 +32,24 @@ echo install doxygen
 
 echo -----------------------------------------------------
 
-git clone -b master --recurse-submodules --depth 1 https://github.com/mingodad/CocoR-CPP
-git clone -b master --recurse-submodules --depth 1 https://github.com/pybind/pybind11
-git clone -b master --recurse-submodules --depth 1 https://github.com/joboccara/NamedType
-git clone -b master --recurse-submodules --depth 1 https://github.com/randombit/botan
-git clone -b master --recurse-submodules --depth 1 https://github.com/madler/zlib
-git clone -b master --recurse-submodules --depth 1 https://github.com/skypjack/entt
-git clone -b master --recurse-submodules --depth 1 https://github.com/chriskohlhoff/asio
-git clone -b main   --recurse-submodules --depth 1 https://github.com/libsdl-org/SDL
-git clone -b master --recurse-submodules --depth 1 https://github.com/opencv/opencv
-git clone -b master --recurse-submodules --depth 1 https://github.com/ocornut/imgui.git
-git clone -b master --recurse-submodules --depth 1 https://github.com/codeplaysoftware/computecpp-sdk.git
+clone_into_external master https://github.com/mingodad/CocoR-CPP
+clone_into_external master https://github.com/pybind/pybind11 
+clone_into_external master https://github.com/joboccara/NamedType 
+clone_into_external master https://github.com/randombit/botan
+clone_into_external master https://github.com/madler/zlib
+clone_into_external master https://github.com/skypjack/entt
+clone_into_external master https://github.com/chriskohlhoff/asio
+clone_into_external main   https://github.com/libsdl-org/SDL
+clone_into_external master https://github.com/opencv/opencv
+clone_into_external master https://github.com/ocornut/imgui
+clone_into_external master https://github.com/codeplaysoftware/computecpp-sdk
 
 echo -----------------------------------------------------
 
-pip install -v "numpy==1.24"
-pip install -v "pandas==1.5"
-pip install -v "matplotlib==3.6"
-pip install -v "seaborn==0.12"
+#pip install -v "numpy==1.24"
+#pip install -v "pandas==1.5"
+#pip install -v "matplotlib==3.6"
+#pip install -v "seaborn==0.12"
 
 echo -----------------------------------------------------
 
