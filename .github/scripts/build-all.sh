3ed53f6dce28788b7486a74c7fd30fd83524cf04
Volker Dörr <68968598+volkerdoerr@users.noreply.github.com>
2023-01-08 15:27:13 +0100

    Update build-all.sh
    Signed-off-by: Volker Dörr <68968598+volkerdoerr@users.noreply.github.com>

--- .github/scripts/build-all.sh
+++ .github/scripts/build-all.sh
@@ -1,6 +1,2 @@
-echo build Coco/R scanner/parser generator
-echo generate DSL scanner/parser sources for all *.atg
-echo build example-dsl-cli
-echo build example-gui
-echo build example-static-library
-echo build example-dynamic-library
+echo build Coco/R executable
+./CocoR-CPP/src/build.sh
