d01583cbd036f3eaa7ab5ac676b51b4fcdded9b4
Volker Dörr <gitlab@cascade.de>
2023-01-09 12:09:42 +0100

    workflow changes

--- .github/scripts/install-all.sh
+++ .github/scripts/install-all.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# attention: script assumes to have the repo-root-folder as current directory!
+
+# attention: script assumes to have the repo-root as current directory!
 
 # external components will be either installed 
 #   - regularily for the operationg system
-#   - or cloned into ../external relative to the repository-root directory 
+#   - or cloned into external in the repository-root
 
 #---------------------------------------------------------
 
@@ -11,38 +12,32 @@
 clone_into_external() {
   # $1: branch
   # $2: repo-path
-  mkdir -p ../external
-  cd ../external
+  mkdir -p external
+  cd external  
   git clone -b $1 --recurse-submodules --depth 1 $2 
+  cd ..
 }
 
 #---------------------------------------------------------
 
-# temporarily go one up:
-mkdir ../external
-cd ../external
-ls
-
-echo -----------------------------------------------------
-
-echo install klee
-echo install catch2
-echo install asciidoctor
-echo install doxygen
+#echo install klee
+#echo install catch2
+#echo install asciidoctor
+#echo install doxygen
 
 echo -----------------------------------------------------
 
 clone_into_external master https://github.com/mingodad/CocoR-CPP
-clone_into_external master https://github.com/pybind/pybind11 
-clone_into_external master https://github.com/joboccara/NamedType 
-clone_into_external master https://github.com/randombit/botan
-clone_into_external master https://github.com/madler/zlib
-clone_into_external master https://github.com/skypjack/entt
-clone_into_external master https://github.com/chriskohlhoff/asio
-clone_into_external main   https://github.com/libsdl-org/SDL
-clone_into_external master https://github.com/opencv/opencv
-clone_into_external master https://github.com/ocornut/imgui
-clone_into_external master https://github.com/codeplaysoftware/computecpp-sdk
+#clone_into_external master https://github.com/pybind/pybind11 
+#clone_into_external master https://github.com/joboccara/NamedType 
+#clone_into_external master https://github.com/randombit/botan
+#clone_into_external master https://github.com/madler/zlib
+#clone_into_external master https://github.com/skypjack/entt
+#clone_into_external master https://github.com/chriskohlhoff/asio
+#clone_into_external main   https://github.com/libsdl-org/SDL
+#clone_into_external master https://github.com/opencv/opencv
+#clone_into_external master https://github.com/ocornut/imgui
+#clone_into_external master https://github.com/codeplaysoftware/computecpp-sdk
 
 echo -----------------------------------------------------
 
