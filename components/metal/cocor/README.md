afa8ce1bd8f9c62c18decc97c70198f6039f88b4
Volker Dörr <gitlab@cascade.de>
2023-01-10 10:14:20 +0100

    submodule CocoR-CPP

--- components/metal/cocor/README.md
+++ components/metal/cocor/README.md
@@ -1,3 +1,4 @@
 # Coco/R
 
-Build the executable
+- build the cocor executable using cmake
+- based on the submodule [CocoR-CPP][CocoR-CPP]
--- components/metal/cocor/build-cocor.sh
+++ components/metal/cocor/build-cocor.sh
@@ -3,14 +3,14 @@
 # set current directory to script source directory:
 cd $(dirname $0)
 
-# clone Coco/R repo into /tmp
-git clone -b master --depth 1 $2 https://github.com/mingodad/CocoR-CPP tmp
+# clone Coco/R repo into /tmp (currently not neccessary as it is a git submodule)
+# git clone -b master --depth 1 $2 https://github.com/mingodad/CocoR-CPP 
 
 # build Coco/R:
-cmake ./ -B tmp/cmake_out
-cmake --build tmp/cmake_out
+cmake ./ -B ~/cmake_out/cocor
+cmake --build ~/cmake_out/cocor
 
 # execute it:
-./tmp/cmake_out/cocor 
+~/cmake_out/cocor/cocor 
 
 
