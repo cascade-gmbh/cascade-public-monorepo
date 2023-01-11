a7767f176867f763ff8bb53444bb8bbfec3a1d08
Volker Dörr <68968598+volkerdoerr@users.noreply.github.com>
2023-01-11 18:55:21 +0100

    Update deploy-all.sh
    Signed-off-by: Volker Dörr <68968598+volkerdoerr@users.noreply.github.com>

--- .github/scripts/deploy-all.sh
+++ .github/scripts/deploy-all.sh
@@ -1,1 +1,12 @@
-echo execute automatic deployment
+#!/bin/bash
+
+# set current directory to script source directory:
+cd $(dirname $0)
+
+echo ------------------------------------------------------- DEPLOY-ALL
+
+#  Coco/R executable:
+wget --no-verbose "https://github.com/octo-org/octo-repo/releases/new?prerelease=1"
+
+echo ------------------------------------------------------- DEPLOY END
+
