3e47c721b5280596ad284f300014e7087334ba9a
Volker Dörr <68968598+volkerdoerr@users.noreply.github.com>
2023-01-12 21:17:16 +0100

    Update build-all.sh
    Signed-off-by: Volker Dörr <68968598+volkerdoerr@users.noreply.github.com>

--- .github/scripts/build-all.sh
+++ .github/scripts/build-all.sh
@@ -6,13 +6,13 @@ cd $(dirname $0)
 echo ------------------------------------------------------- BUILD-ALL
 
 # build Coco/R executable:
-#../../components/metal/cocor/build.sh
+../../components/metal/cocor/build.sh
 
 echo ------------------------------------------------------- BUILD-ALL END
 
 # show resulting build directory:
 echo RESULTS:
-#echo $PROJECT_BUILD_HOME
-#ls -gpF $PROJECT_BUILD_HOME
+echo $PROJECT_BUILD_HOME
+ls -gpF $PROJECT_BUILD_HOME
 
 
