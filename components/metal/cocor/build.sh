23b636911fa710977f4f979f1eb3fd0d9f07ddad
Volker Dörr <68968598+volkerdoerr@users.noreply.github.com>
2023-01-12 21:49:44 +0100

    Update build.sh
    Signed-off-by: Volker Dörr <68968598+volkerdoerr@users.noreply.github.com>

--- components/metal/cocor/build.sh
+++ components/metal/cocor/build.sh
@@ -8,24 +8,27 @@ echo ------------------------------------------------------- build-cocor
 # clone Coco/R repo into /tmp (currently not neccessary as it is a git submodule)
 # git clone -b master --depth 1 $2 https://github.com/mingodad/CocoR-CPP 
 
-invoke cmake
+# invoke cmake
 echo BUILD-COCOR:
 cmake ./ -B $PROJECT_BUILD_HOME/cocor
 cmake --build $PROJECT_BUILD_HOME/cocor
 
-show resulting build directory
+# show resulting build directory
 echo BUILD RESULTS:
 echo $PROJECT_BUILD_HOME/cocor
 ls -gpF --color $PROJECT_BUILD_HOME/cocor
 
 # simple test
 echo COCOR TEST-CALL WITHOUT PARAMETERS:
-if [ -f "$PROJECT_BUILD_HOME/cocor/cocor
-then
-   $PROJECT_BUILD_HOME/cocor/cocor
-else
-   $PROJECT_BUILD_HOME/cocor/cocor.exe
-fi
+$PROJECT_BUILD_HOME/cocor/cocor
+
+
+#if [ -f "$PROJECT_BUILD_HOME/cocor/cocor
+#then
+ #  $PROJECT_BUILD_HOME/cocor/cocor
+#else
+   #$PROJECT_BUILD_HOME/cocor/cocor.exe
+#fi
  
 
 
